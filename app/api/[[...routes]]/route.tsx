1da62d494729b0ba800aac46ad1220da7fb2eff4
Milena <milena@Milenas-MacBook-Air.local>
2024-09-16 03:39:59 -0400

    ramdon and random by error

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -37,12 +37,12 @@ app.frame('/0', (c) => {
   })
 })
 
-app.frame('/1', (c) => {
+app.frame('/1', (c) => { //good delegate
   const { buttonValue, inputText, status } = c;
   const delegate = inputText || buttonValue || ''; 
 
   return c.res({
-    action: '/1',
+    action: '/2',
     image: (
       <div style={{
         display: 'flex',
@@ -84,12 +84,209 @@ app.frame('/1', (c) => {
       </div>
     ),
     intents: [
+      <Button value="next">next</Button>,
   
       status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   });
 });
 
+app.frame('/2', (c) => { // random recommendation
+  const { buttonValue, inputText, status } = c
+  const delegate = inputText || buttonValue
+  
+  return c.res({
+    action: '/3',
+    image: (  
+      <div
+        style={{
+          display: 'flex',
+          background: '#f6f6f6',
+          alignItems: 'center',
+          position: 'relative',
+        }}
+      > 
+        <img width="1200" height="630" alt="background" src={`/2.png`} />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'absolute',
+            color: '#161B33',
+            fontSize: '65px',
+            textTransform: 'uppercase',
+            letterSpacing: '-0.030em',
+            width: '100%',
+            boxSizing: 'border-box',
+            alignItems: 'center',
+            lineHeight: 0.8,
+            padding: '0px 50px',
+            overflow: 'hidden', 
+            textOverflow: 'ellipsis',
+            textAlign: 'center', 
+            top: '30%',
+            height: '80%',
+          }}>      
+          {/* Container for items */}
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row', // Items in one row
+            justifyContent: 'space-between', // Space items evenly
+            width: '100%',
+            maxWidth: '100%',
+          }}>
+            {/* Item 1 */}
+            <div style={{
+              color: '#36A4B4',
+              marginLeft: '8%',
+              marginRight: '0',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              height: 'auto',
+            }}>                    
+              item 1
+            </div>
+
+            {/* Item 2 centered */}
+            <div style={{
+              color: '#E5383B',
+              marginLeft: '3%',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              height: 'auto',
+            }}>                    
+              item 2
+            </div>
+
+            {/* Item 3 */}
+            <div style={{
+              color: '#36A4B4',
+              margin: '0 50px', // Space for item 3
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              height: 'auto',
+            }}>                    
+              item 3
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      ),
+    intents: [
+      <Button value="next">next</Button>,
+     
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+
+  
+
+app.frame('/3', (c) => { //reccomendation by social graph 
+  const { buttonValue, inputText, status } = c
+  const delegate = inputText || buttonValue
+  return c.res({
+    action: '/4',
+    image: 'http://localhost:3000/7.png',
+    intents: [
+      <Button value="next">next</Button>,
+     
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+app.frame('/4', (c) => { //random by error 
+  const { buttonValue, inputText, status } = c
+  const delegate = inputText || buttonValue
+  
+  return c.res({
+    action: '/0',
+    image: (  
+      <div
+        style={{
+          display: 'flex',
+          background: '#f6f6f6',
+          alignItems: 'center',
+          position: 'relative',
+        }}
+      > 
+        <img width="1200" height="630" alt="background" src={`/4.png`} />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'absolute',
+            color: '#161B33',
+            fontSize: '65px',
+            textTransform: 'uppercase',
+            letterSpacing: '-0.030em',
+            width: '100%',
+            boxSizing: 'border-box',
+            alignItems: 'center',
+            lineHeight: 0.8,
+            padding: '0px 50px',
+            overflow: 'hidden', 
+            textOverflow: 'ellipsis',
+            textAlign: 'center', 
+            top: '30%',
+            height: '80%',
+          }}>      
+          {/* Container for items */}
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row', // Items in one row
+            justifyContent: 'space-between', // Space items evenly
+            width: '100%',
+            maxWidth: '100%',
+          }}>
+            {/* Item 1 */}
+            <div style={{
+              color: '#36A4B4',
+              marginLeft: '8%',
+              marginRight: '0',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              height: 'auto',
+            }}>                    
+              item 1
+            </div>
+
+            {/* Item 2 centered */}
+            <div style={{
+              color: '#E5383B',
+              marginLeft: '3%',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              height: 'auto',
+            }}>                    
+              item 2
+            </div>
+
+            {/* Item 3 */}
+            <div style={{
+              color: '#36A4B4',
+              margin: '0 50px', // Space for item 3
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              height: 'auto',
+            }}>                    
+              item 3
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      ),
+    intents: [
+      <Button value="next">next</Button>,
+     
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
 
 devtools(app, { serveStatic });
 
